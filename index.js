90e174c1ac0380be7179ac1def64895101b68af8
Sammwy <44925968+sammwyy@users.noreply.github.com>
2019-10-03 21:59:39 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-const newerestVersion = '4.10.1503.4';
-const versionsAllowed = [newerestVersion, '4.10.1503.4'];
-
-exports.inject = (app, version) => {
-
-	if (version == null)
-		version = newerestVersion;
-
-	if (!versionsAllowed.includes(version))
-		version = newerestVersion;
-
-	app.commandLine.appendSwitch('widevine-cdm-path', __dirname + '/plugin/' + version);
-	app.commandLine.appendSwitch('widevine-cdm-version', version);
+exports.inject = (app) => {
+	app.commandLine.appendSwitch('widevine-cdm-path', __dirname + '/plugin/');
+	app.commandLine.appendSwitch('widevine-cdm-version', '4.10.1503.4');
 }
